ec4192b63fe9d707f230a30e04b837d678fa87d7
gabiediasalves <gabiediasalves@gmail.com>
2022-09-27 14:52:10 -0300

    updating functions

--- src/components/pages/NewProject.jsx
+++ src/components/pages/NewProject.jsx
@@ -1,12 +1,34 @@
+import { useNavigate } from 'react-router-dom';
 import ProjectForm from "../project/ProjectForm"
 import styles from "./NewProject.module.css"
 
 function NewProject() {
+
+  const navigate = useNavigate();
+
+  function createPost(project) {
+    //inicializa cost e services
+    project.cost = 0
+    project.services = []
+
+    fetch('http:/localhost:5000/projects', {
+      method: "POST",
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(project)
+    })
+      .then((resp) => resp.json())
+      .then((data) => { console.log(data) })
+      .catch(err => console.log(err))
+
+  }
+
   return (
     <div className={styles.newproject_container}>
       <h1>Criar Projeto</h1>
       <p>Crie seu projeto para depois adicionar os serviços</p>
-    <ProjectForm btnText="Criar projetos"/>
+      <ProjectForm handleSubmit={createPost} btnText="Criar projetos" />
     </div>
   )
 }
--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -5,9 +5,10 @@ import Select from '../form/Select'
 import Submit from '../form/Submit'
 import styles from './Project.module.css'
 
-function ProjectForm({ btnText }) {
+function ProjectForm({ handleSubmit, btnText, projectData }) {
 
   const [categories, setCategories] = useState([])
+  const [project, setProject] = useState(projectData || {})
 
   useEffect(() => {
     fetch("http://localhost:5000/categories", {
@@ -21,21 +22,33 @@ function ProjectForm({ btnText }) {
         setCategories(data)
       })
       .catch((err) => console.log(err))
-  })
+  }, [])
+
+  const submit = (e) => {
+    e.preventDefault()
+    handleSubmit(project)
+  }
+
+  function handleChange(e){
+    setProject({...project, [e.target.name]: e.target.value})
+    console.log(project)
+  }
 
   return (
-    <form className={styles.form}>
+    <form onSubmit={submit} className={styles.form}>
       <Input
         type="text"
         text="Nome do projeto"
         name="name"
         placeholder="Insira o nome do novo projeto"
+        handleOnChange={handleChange}
       />
       <Input
         type="number"
         text="Orçamento do projeto"
         name="budget"
         placeholder="Insira o orçamento total"
+        handleOnChange={handleChange}
       />
       <Select
         name="category_id"
