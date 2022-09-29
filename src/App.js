05b7b63f28afab1d5bd161336c3b0938f93c3ca5
gabiediasalves <gabiediasalves@gmail.com>
2022-09-28 22:12:39 -0300

    add editPost and messages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home';
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project';
 import Company from './components/pages/Company';
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject';
@@ -14,11 +15,12 @@ function App() {
     <Navbar/>
       <Container customClass="min-height">
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/projects' element={<Projects />} />
-          <Route exact path='/company' element={<Company />} />
-          <Route exact path='/contact' element={<Contact />} />
-          <Route exact path='/newproject' element={<NewProject />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/company' element={<Company />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/newproject' element={<NewProject />} />
+          <Route path='/project/:id' element={<Project />} />
         </Routes>
       </Container>
       <Footer/>
